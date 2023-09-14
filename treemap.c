472fa4cc3ceb70bf2ab99c8e0a3172dbb4d7103e
Replit user <>
2023-09-14 02:43:25 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -138,7 +138,15 @@ Pair * upperBound(TreeMap * tree, void* key) {
 }
 
 Pair * firstTreeMap(TreeMap * tree) {
+  if (tree->root == NULL) return NULL;
+  tree->current = tree->root;
+  while (tree->current->left != NULL){
+    tree->current = tree->current->left;
+  }
+  return tree->current->pair;
 }
+
+
 
 Pair * nextTreeMap(TreeMap * tree) {
 }
